ecfad831c15a677447d45543a966f3c68825f966
olekxdd <jumbalaya@hotmail.de>
2023-10-05 14:36:31 +0200

    test

--- shift register and 7segment/tester.py
+++ shift register and 7segment/tester.py
@@ -1,110 +1,128 @@
 import gpiozero
 from gpiozero import LED, output_devices
 from time import sleep
-
-# led = LED(23)
-# x = 0
-# while x < 5:
 #
-#     led.on()
-#     time.sleep(1)
-#     led.off()
-#     x += 1
-#     time.sleep(1)
-
-# SN74HC595N light row and 7 segment
-
-# Outputs:
-# VCC 5V(pin2)
-# GND (Pin6)
-# DS (4)
-# SHCP (23)
-# STCP (24)
-# OE into GND
-
-# if SHCP is high, a low is safed in the buffer
-# A high is safed in the buffer when SHCP is high WHILE DS is high
-# to transfer the signal STCP must be high (under the cicumstance that OE is grounded to begin with)
-
-# data = [0, 1, 0, 1, 0, 0, 0, 1]
+# # led = LED(23)
+# # x = 0
+# # while x < 5:
+# #
+# #     led.on()
+# #     time.sleep(1)
+# #     led.off()
+# #     x += 1
+# #     time.sleep(1)
 #
-# numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-# numbers_byte = [11000000, 11111001, 10100100, 10110000, 10011001, 10010010, 10000010, 11111000, 10000000, 10010000]
-# num = int(input("input a number between 1-9"))
-# # def segmenter():
-# #     for i in numbers:
-# #         numbers[i] = numbers_byte[i]
-# #         print(numbers[i])
+# # SN74HC595N light row and 7 segment
 #
-# def segmenter1(num):
-#     while num == numbers[num]:
-#         for i in numbers:
-#             numbers[i] = numbers_byte[i]
-#             print(numbers[i])
-#     else:
-#         print("number must be between 0-9")
+# # Outputs:
+# # VCC 5V(pin2)
+# # GND (Pin6)
+# # DS (4)
+# # SHCP (23)
+# # STCP (24)
+# # OE into GND
 #
-# segmenter1(num)
+# # if SHCP is high, a low is safed in the buffer
+# # A high is safed in the buffer when SHCP is high WHILE DS is high
+# # to transfer the signal STCP must be high (under the cicumstance that OE is grounded to begin with)
 #
-#
+# # data = [0, 1, 0, 1, 0, 0, 0, 1]
+# #
+# # numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# # numbers_byte = [11000000, 11111001, 10100100, 10110000, 10011001, 10010010, 10000010, 11111000, 10000000, 10010000]
+# # num = int(input("input a number between 1-9"))
+# # # def segmenter():
+# # #     for i in numbers:
+# # #         numbers[i] = numbers_byte[i]
+# # #         print(numbers[i])
+# #
+# # def segmenter1(num):
+# #     while num == numbers[num]:
+# #         for i in numbers:
+# #             numbers[i] = numbers_byte[i]
+# #             print(numbers[i])
+# #     else:
+# #         print("number must be between 0-9")
+# #
+# # segmenter1(num)
+# #
+# #
 # ds_data = 0
 # shcp_inst_shifter = 0
 # stcp_outputter = 0
+# #
 #
-
-
-# def data_processor(data):
-# #    if len(data) > 6
+#
+# # def data_processor(data):
+# # #    if len(data) > 6
+# #     for i in data:
+# #         if data[i] == 1:
+# #             ds_data = 1
+# #             shcp_inst_shifter = 1
+# #             shcp_inst_shifter = 0
+# #             ds_data = 0
+# #             stcp_outputter = 1
+# #             stcp_outputter = 0
+# #             print("eins")
+# #         else:
+# #             shcp_inst_shifter = 1
+# #             shcp_inst_shifter = 0
+# #             stcp_outputter = 1
+# #             stcp_outputter = 0
+# #             print("null")
+# #
+# # data_processor(data)
+#
+#
+# list = [124124, 45845, 1767547]
+# print(list[0][1])
+#
+#
+#
+# def data_processor(data=(segmenter1()), single_byte=None):
 #     for i in data:
-#         if data[i] == 1:
-#             ds_data = 1
-#             shcp_inst_shifter = 1
-#             shcp_inst_shifter = 0
-#             ds_data = 0
-#             stcp_outputter = 1
-#             stcp_outputter = 0
-#             print("eins")
+#         data[i] = single_byte
+#         if single_byte[i] == 1:
+#             ds_data.value(1)
+#             shcp_inst_shifter.value(1)
+#             shcp_inst_shifter.value(0)
+#             ds_data.value(0)
+#             stcp_outputter.value(1)
+#             stcp_outputter.value(0)
+#             print("null")
 #         else:
-#             shcp_inst_shifter = 1
-#             shcp_inst_shifter = 0
-#             stcp_outputter = 1
-#             stcp_outputter = 0
+#             shcp_inst_shifter.value(1)
+#             shcp_inst_shifter.value(0)
+#             stcp_outputter.value(1)
+#             stcp_outputter.value(0)
 #             print("null")
 #
-# data_processor(data)
+#
+#
+# # def segmenter1():
+# #     num = int(input("input a number between 0-9: "))
+# #     while num == numbers[num]:
+# #         for i in numbers:
+# #             numbers[i] = int(numbers_byte[i])
+# #             print(numbers[i])
+# #         break
+# #     else:
+# #         print("number must be between 0-9")
+
+#ds_data = gpiozero.GPIODevice(4, pin_factory=None)
+shcp_inst_shifter = gpiozero.GPIODevice(26, pin_factory=None)
+#stcp_outputter = gpiozero.GPIODevice(24, pin_factory=None)
 
 
-list = [124124, 45845, 1767547]
-print(list[0][1])
-
-
-
-def data_processor(data=(segmenter1()), single_byte=None):
-    for i in data:
-        data[i] = single_byte
-        if single_byte[i] == 1:
-            ds_data.value(1)
-            shcp_inst_shifter.value(1)
-            shcp_inst_shifter.value(0)
-            ds_data.value(0)
-            stcp_outputter.value(1)
-            stcp_outputter.value(0)
-            print("null")
-        else:
-            shcp_inst_shifter.value(1)
-            shcp_inst_shifter.value(0)
-            stcp_outputter.value(1)
-            stcp_outputter.value(0)
-            print("null")
-
-
-
-# def segmenter1():
-#     num = int(input("input a number between 0-9: "))
-#     while num == numbers[num]:
-#         for i in numbers:
-#             numbers[i] = int(numbers_byte[i])
-#             print(numbers[i])
-#         break
-#     else:
-#         print("number must be between 0-9")
+shcp_inst_shifter.value(1)
+sleep(1)
+shcp_inst_shifter.value(0)
+sleep(1)
+shcp_inst_shifter.value(1)
+sleep(1)
+shcp_inst_shifter.value(0)
+sleep(1)
+shcp_inst_shifter.value(1)
+sleep(1)
+shcp_inst_shifter.value(0)
+sleep(1)

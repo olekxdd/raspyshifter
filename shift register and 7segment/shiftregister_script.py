db08589061868725c8c10aab01ad23e15acbedb7
olekxdd <jumbalaya@hotmail.de>
2023-10-10 16:51:05 +0200

    test

--- shift register and 7segment/shiftregister_script.py
+++ shift register and 7segment/shiftregister_script.py
@@ -2,10 +2,12 @@ from gpiozero import DigitalOutputDevice, DigitalInputDevice
 from gpiozero import LED, output_devices
 from time import sleep
 
-ds_data = 24
+ds_data = 4
 shcp_inst_shifter = 23
-stcp_outputter = 18
+stcp_outputter = 24
+oe_pullup = 26
 
+oe_pin = DigitalInputDevice(oe_pullup)
 data_pin = DigitalOutputDevice(ds_data)
 shcp_pin = DigitalOutputDevice(shcp_inst_shifter)
 stcp_pin = DigitalOutputDevice(stcp_outputter)

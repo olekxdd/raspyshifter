f10e24703392fd1d225611764dd27d82c7086054
olekxdd <jumbalaya@hotmail.de>
2023-11-08 19:38:17 +0100

    123

---
+++ Hot stepper5/Stepper motor control.py
@@ -0,0 +1,36 @@
+from gpiozero import DigitalOutputDevice, DigitalInputDevice, LED, output_devices, motor
+from time import sleep
+import random
+from signal import pause
+
+delay = 0.05
+steps = 500
+
+fullsteps = ((0,1,0,1), (0,1,1,0), (1,0,1,0), (1,0,0,1))
+
+halfsteps = ((0,1,0,1), (0,1,0,0), (0,1,1,0), (0,0,1,0),
+             (1,0,1,0), (1,0,0,0), (1,0,0,1), (0,0,0,1))
+
+coil_a1_pin = 18 #color
+coil_a2_pin = 4 #color
+coil_b1_pin = 15 #color
+coil_b2_pin = 14 #color
+
+# Creating physical Pins with the GPIOZero Libary
+
+coil_A1 = DigitalOutputDevice(coil_a1_pin)
+coil_A2 = DigitalOutputDevice(coil_a2_pin)
+coil_B1 = DigitalOutputDevice(coil_b1_pin)
+coil_B2 = DigitalOutputDevice(coil_b2_pin)
+
+def stepSequence(w1, w2, w3, w4):
+    coil_A1.value(w1)
+    coil_A2.value(w2)
+    coil_B1.value(w3)
+    coil_B2.value(w4)
+
+for i in range(0, steps):
+    for pattern in fullsteps:
+        stepSequence(pattern)
+        sleep(delay)
+
--- Leetcode/2200. Find All K-Distant Indices in an Array.py
+++ Leetcode/2200. Find All K-Distant Indices in an Array.py
@@ -6,20 +6,52 @@ k = 1
 
 
 # print(list(enumerate(nums)))
+# for every index of the key nums[j]
+# you have to check if the distance abs(i - j) is in the constraints 1 <= distance <= len(nums) and nums[j] == key
+list = []
+for j, e in enumerate(nums):
+    if e == key:
+        for i in range(len(nums)):
+            distance = abs(i - j)
+            if 1 <= k <= len(nums) and nums[j] == key:
+                list.append(distance)
+
+print(sorted(list))
+
+# distance = abs(i - j)
+# if 1 <= distance <= len(nums) and nums[j] == key:
 
 
-def func_k_distances(nums):
-    list = []
-    for i in range(len(nums)):
-        for j in range(len(nums)):
-            if j == key:
-                distance = abs(i - j)
-                if 1 <= distance <= len(nums) and nums[j] == key:
-                    list.append(distance)
-    print(sorted(list))
 
 
-func_k_distances(nums)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def func_k_distances(nums):
+#     list = []
+#     for i in range(len(nums)):
+#         for j in range(len(nums)):
+#             if j == key:
+#                 distance = abs(i - j)
+#                 if 1 <= distance <= len(nums) and nums[j] == key:
+#                     list.append(distance)
+#     print(sorted(list))
+
+
+
 
 # def func_k_distances(nums):
 #     list = []

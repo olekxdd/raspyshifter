577d6f3ef9f921f7ad674bfd7328daf342aa465c
olekxdd <jumbalaya@hotmail.de>
2023-11-08 19:50:34 +0100

    123

--- shift register and 7segment/tester.py
+++ shift register and 7segment/tester.py
@@ -7,15 +7,15 @@ from signal import pause
 
 ds_data = 26
 
-n = 1
+n = 0.1
 # Creating physical Pins with the GPIOZero Libary
 
 data_pin = DigitalOutputDevice(ds_data)
 while True:
 
-    y = math.sin(n)
+    y = abs(math.sin(n))
     data_pin.value = 1
     sleep(y)
     data_pin.value = 0
     sleep(y)
-    n += 1
+    n += 0.1

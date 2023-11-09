151675982237c0818b06355b28c87004151c0d95
olekxdd <jumbalaya@hotmail.de>
2023-11-09 17:15:28 +0100

    added documentatio

--- Hot stepper5/Stepper motor control.py
+++ Hot stepper5/Stepper motor control.py
@@ -6,10 +6,14 @@ from signal import pause
 delay = 0.05
 steps = 500
 
+# stepping modes (w1, w2, w3, w4) w1 = coil one
+
 fullsteps = ((0,1,0,1), (0,1,1,0), (1,0,1,0), (1,0,0,1))
 
 halfsteps = ((0,1,0,1), (0,1,0,0), (0,1,1,0), (0,0,1,0),
              (1,0,1,0), (1,0,0,0), (1,0,0,1), (0,0,0,1))
+
+# declaring pins
 
 coil_a1_pin = 18 #color
 coil_a2_pin = 4 #color
@@ -23,13 +27,14 @@ coil_A2 = DigitalOutputDevice(coil_a2_pin)
 coil_B1 = DigitalOutputDevice(coil_b1_pin)
 coil_B2 = DigitalOutputDevice(coil_b2_pin)
 
+# function to control and apply the stepping sequence pattern
 def stepSequence(w1, w2, w3, w4):
     coil_A1.value = w1
     coil_A2.value = w2
     coil_B1.value = w3
     coil_B2.value = w4
 
-
+# controls the duration of the sequence and feeds the pattern into the function
 
 for i in range(0, steps):
     for pattern in fullsteps:

bbb6a828a5c00e5198ae8cc209c8489df4b97abb
olekxdd <jumbalaya@hotmail.de>
2023-11-20 14:31:13 +0100

    added documentatio

--- Hot stepper5/Stepper motor control.py
+++ Hot stepper5/Stepper motor control.py
@@ -1,24 +1,25 @@
-from gpiozero import DigitalOutputDevice, DigitalInputDevice, LED, output_devices, motor
+from gpiozero import OutputDevice, DigitalOutputDevice, DigitalInputDevice, LED, output_devices, motor,
 from time import sleep
+import numpy as np
 import random
 from signal import pause
 
 delay = 0.05
-steps = 500
-
+steps_amount = 500
+ramp_up_steps = 100
 # stepping modes (w1, w2, w3, w4) w1 = coil one
 
-fullsteps = ((0,1,0,1), (0,1,1,0), (1,0,1,0), (1,0,0,1))
+fullsteps = ((0, 1, 0, 1), (0, 1, 1, 0), (1, 0, 1, 0), (1, 0, 0, 1))
 
-halfsteps = ((0,1,0,1), (0,1,0,0), (0,1,1,0), (0,0,1,0),
-             (1,0,1,0), (1,0,0,0), (1,0,0,1), (0,0,0,1))
+halfsteps = ((0, 1, 0, 1), (0, 1, 0, 0), (0, 1, 1, 0), (0, 0, 1, 0),
+             (1, 0, 1, 0), (1, 0, 0, 0), (1, 0, 0, 1), (0, 0, 0, 1))
 
 # declaring pins
 
-coil_a1_pin = 18 #color
-coil_a2_pin = 4 #color
-coil_b1_pin = 15 #color
-coil_b2_pin = 14 #color
+coil_a1_pin = 18  # color
+coil_a2_pin = 4  # color
+coil_b1_pin = 15  # color
+coil_b2_pin = 14  # color
 
 # Creating physical Pins with the GPIOZero Libary
 
@@ -27,17 +28,42 @@ coil_A2 = DigitalOutputDevice(coil_a2_pin)
 coil_B1 = DigitalOutputDevice(coil_b1_pin)
 coil_B2 = DigitalOutputDevice(coil_b2_pin)
 
+
 # function to control and apply the stepping sequence pattern
-def stepSequence(w1, w2, w3, w4):
+def step_sequence(w1, w2, w3, w4):
     coil_A1.value = w1
     coil_A2.value = w2
     coil_B1.value = w3
     coil_B2.value = w4
 
+
+def speed_control(ramp_up_steps, ):
+
+
 # controls the duration of the sequence and feeds the pattern into the function
+def motor_run_time(steps, stepmode):
+    for i in range(0, steps):
+        for pattern in stepmode:
+            step_sequence(pattern)
+            sleep(delay)
 
-for i in range(0, steps):
-    for pattern in fullsteps:
-        stepSequence(pattern)
-        sleep(delay)
+motor_run_time(steps_amount, fullsteps)
 
+max_steps_per_second = 1000 # 15,625 umdrehungen pro sekunde
+max_speed = 1
+initial_speed = 0
+
+
+a = 0.001
+b = 0.001
+c = 0.01
+
+rate_of_change = 1
+
+def accel_func(x):
+
+    z = np.exp(-c * x)
+    sig = 1 / ((a * 1) + b * z)
+    time_step = 1/sig
+
+    return time_step

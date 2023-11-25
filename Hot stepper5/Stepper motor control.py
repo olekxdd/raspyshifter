02a10ad40b05059245e3e412fa8a7aa71edd6fef
olekxdd <jumbalaya@hotmail.de>
2023-11-25 19:05:26 +0100

    added documentatio

--- Hot stepper5/Stepper motor control.py
+++ Hot stepper5/Stepper motor control.py
@@ -71,8 +71,8 @@ def motor_run_time(ramp_start_step, ramp_end_step, stepmode):
     for i in range(ramp_start_step, ramp_end_step):
         for pattern in stepmode:
             step_sequence(*pattern)
-            sleep((1000 / (accel_func(i, a, b, c) * 64))+100)
-            print((1000 / (accel_func(i, a, b, c) * 64))+100)
+            sleep((1000 / (accel_func(i, a, b, c) * 64)))
+            print((1000 / (accel_func(i, a, b, c) * 64)))
 
 # def motor_run_time(ramp_start_step, ramp_end_step, stepmode):
 #     for i in range(ramp_start_step, ramp_end_step):
